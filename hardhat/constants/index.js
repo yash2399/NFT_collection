1cd486e27c188cfe144656de21c1c4a89e5c0e86
Yash Saraswat <yash@Yashs-MacBook-Pro-3.local>
2022-12-22 16:13:52 +0530

    first commit

---
+++ hardhat/constants/index.js
@@ -0,0 +1,5 @@
+const WHITELIST_CONTRACT_ADDRESS = "0xac5d69b2c34584901b0e3feb549aab6a31c75bb9";
+
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
